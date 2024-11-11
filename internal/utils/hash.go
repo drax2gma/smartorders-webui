047d105058f347fc5ba186266aa7df2abdfc4ec4
Mark G <drax2gma@gmail.com>
2024-11-11 13:18:42 +0100

    up

--- internal/utils/hash.go
+++ internal/utils/hash.go
@@ -6,7 +6,11 @@ import (
 	"github.com/cespare/xxhash/v2"
 )
 
+const (
+	masterSalt = "be0d748d-58e2-50d5-b194-8996858d97ba-7d30b755-e3ec-5f0d-a2e8-0a003198c34d"
+)
+
 func GenerateXXH64Hash(input string) string {
-	hash := xxhash.Sum64String(input)
+	hash := xxhash.Sum64String(input + masterSalt)
 	return fmt.Sprintf("%016x", hash)
 }
